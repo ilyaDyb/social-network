bfdc3503cc95c69152d9b38c52265fead6d54fbe
Ilya Dyb <ilya77788899@mail.ru>
2024-06-11 13:41:28 +0300

    add auto-deleting Temprory Users with pereodic tasks

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Users, UserProfile, Friendship
+from .models import TemporaryUser, Users, UserProfile, Friendship
 
 @admin.register(Users)
 class UserAdmin(admin.ModelAdmin):
@@ -7,4 +7,5 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ["id", "username", "first_name", "last_name", "email"]
 admin.site.register(UserProfile)
 admin.site.register(Friendship)
+admin.site.register(TemporaryUser)
 
